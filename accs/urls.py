0d024d933a3c5f95d7cdd14912d57970bfdb1312
ramid <ramid@qq.com>
2025-03-26 15:16:03 +0800

    feat: 初始化

---
+++ accs/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import RegisterView, LoginView
+
+router = DefaultRouter()
+# router.register(r'XXXX', XXXXX)
+urlpatterns = [
+    path('', include(router.urls)),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+
+]
