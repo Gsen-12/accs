148b33ea7ea60a4c021a61c87d3d375e7b1b5ce3
dy <dy@qq.com>
2025-04-02 10:12:36 +0800

    Reapply "feat: 增加初步登录验证"
    This reverts commit 17c6b3eec04751046f897b1ea5f28b615f03a52e.

--- CorrectionPlatformBackend/settings.py
+++ CorrectionPlatformBackend/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -27,14 +26,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    # 'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    # 'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
@@ -42,22 +41,25 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # 'corsheaders.middleware.CorsMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'accs.utils.middleware.CloseCsrfMiddleware'
 ]
+
+# CSRF_FAILURE_VIEW = 'accs.views.csrf_failure'
 
 ROOT_URLCONF = 'CorrectionPlatformBackend.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,18 +74,38 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'CorrectionPlatformBackend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}'''
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "ACCS",
+        "USER": "root",
+        "PASSWORD": "123456",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "CHARSET": "utf8mb4"
+    }
+
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'accs.authentication.CustomJWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
 
+AUTHENTICATION_BACKENDS = (
+    'accs.views.MyCustomBackend',
+)
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -102,7 +124,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -113,7 +134,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
--- accs/views.py
+++ accs/views.py
@@ -1,21 +1,43 @@
-from django.shortcuts import render
+import time
+import datetime
+
+from django.db.models import Q
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UserSerializer
-from django.contrib.auth import authenticate
+from .serializers import MyTokenObtainPairSerializer
+from rest_framework.permissions import AllowAny
+from accs.models import Roles, BlacklistedToken
+from accs.serializers import UserSerializer
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.http import JsonResponse
+from .utils import token
+
+User = get_user_model()
 
 
+def csrf_failure(request, reason=""):
+    return JsonResponse({"error": "CSRF验证失败"}, status=403)
 # Create your views here.
+
 
 class RegisterView(APIView):
     @staticmethod
     def post(request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        user_serializer = UserSerializer(data=request.data)
+        if user_serializer.is_valid():
+            if request.data.get('role_id') is not None:
+                user_serializer.save()
+                Roles.objects.create(role_id=request.data.get('role_id'), user_id=user_serializer.data.get('id'))
+                return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response({"message": "未获取到role_id"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginView(APIView):
@@ -24,6 +46,50 @@ class LoginView(APIView):
         username = request.data.get('username')
         password = request.data.get('password')
         user = authenticate(username=username, password=password)
+
         if user is not None:
-            return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+            token = MyTokenObtainPairSerializer.get_token(user)
+            # token.set_exp(lifetime=datetime.timedelta())
+            return Response({'message': 'Login successful', 'token': str(token)}, status=status.HTTP_200_OK)
         return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+@csrf_exempt
+class LogoutView(APIView):
+    @csrf_protect
+    def post(self, request):
+        try:
+            refresh_token = request.data.get('refresh_token')
+            if not refresh_token:
+                return Response(
+                    {"error": "缺少 refresh_token"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            # 将 refresh_token 加入黑名单
+            token = RefreshToken(refresh_token)
+            BlacklistedToken.add(token)
+
+            return Response({"message": "退出成功"}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(
+                {"error": "无效的 Token"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        # token = MyTokenObtainPairSerializer.get_token(user)
+        # token.set_exp(lifetime=datetime.timedelta())
+
+
+class MyObtainTokenPairView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
+
+
+# MyCustomBackend
+class MyCustomBackend(TokenObtainPairView):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = User.objects.get(Q(username=username) | Q(email=username))
+            if user.check_password(password):
+                return user
+        except Exception as e:
+            return None
