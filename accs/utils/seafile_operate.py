ce34d7f1a5ecd163e15b2c818f34609a48eb5323
Gsen-12 <Gs-12@qq.com>
2025-06-23 17:05:09 +0800

    feat: UserModificationView and TempAvatarUploadView

--- accs/utils/seafile_operate.py
+++ accs/utils/seafile_operate.py
@@ -17,63 +17,6 @@ class SeafileOperations:
             'Authorization': token
         }
 
-    def get_share_file_by_repo(self, repo_id: str, file_path: str) -> list:
-        url = self.server_url + f'/api/v2.1/share-links/?repo_id={repo_id}&path={file_path}'
-        response = requests.get(url, headers=self.headers)
-        if check_response(response):
-            return response.json()
-
-    def delete_share_file_by_repo(self, repo_id: str, file_path: str) -> list:
-        share_links = self.get_share_file_by_repo(repo_id=repo_id, file_path=file_path)
-        result_list = []
-        for share_link in share_links:
-            # todo：某个文件删除失败
-            url = self.server_url + f"/api/v2.1/share-links/{share_link['token']}"
-            response = requests.delete(url, headers=self.headers)
-            if check_response(response):
-                result_list.append(response.json()['success'])
-        if not result_list:
-            warnings.warn("不存在该文件，结束删除操作")
-
-        return result_list
-
-    def post_share_file_by_repo(self, repo_id: str, file_path: str, password: str = None) -> list:
-
-        url = self.server_url + f'/api/v2.1/share-links/'
-        response = requests.post(
-            url,
-            headers=self.headers,
-            json={
-                'path': file_path,
-                'repo_id': repo_id,
-                'password': password,
-                'permissions': {
-                    'can_edit': False,
-                    'can_download': True
-                }
-            }
-        )
-        if check_response(response):
-            return response.json()
-
-    def post_share_ava_by_repo(self, repo_id: str, file_path: str) -> list:
-
-        url = self.server_url + f'/api/v2.1/share-links/'
-        response = requests.post(
-            url,
-            headers=self.headers,
-            json={
-                'path': file_path,
-                'repo_id': repo_id,
-                'permissions': {
-                    'can_edit': False,
-                    'can_download': True
-                }
-            }
-        )
-        if check_response(response):
-            return response.json()
-
     def get_file_history_by_repo(self, repo_id: str, file_path: str) -> list:
         url = self.server_url + f'/api/v2.1/repos/{repo_id}/file/history/?path={file_path}'
         response = requests.get(
